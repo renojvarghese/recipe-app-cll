05e99b69dad197d01dd19f6873a336ce22542c96
Renoj Varghese <renoj.varghese@uconn.edu>
2018-12-27 17:36:57 -0500

    adding Router and app components

---
+++ recipe-app-cll/src/components/AccountSettings/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class AccountSettings extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>I am the Account Settings Component</h1>
+                <h2>Edit me from src/components/AccountSettings/index.js</h2>
+                <h2>Edit my styles from src/components/App/App.css</h2>
+            </div>
+        )
+    }
+}
---
+++ recipe-app-cll/src/components/LandingPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class LandingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>I am the Landing Page Component</h1>
+                <h2>Edit me from src/components/LandingPage/index.js</h2>
+                <h2>Edit my styles from src/components/App/App.css</h2>
+            </div>
+        )
+    }
+}
