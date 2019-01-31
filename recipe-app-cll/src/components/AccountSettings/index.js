bc25d72cf9e374cd4c845f5c3c55148ad4235620
Thanita Edu <thanitag.io@gmail.com>
2019-01-31 17:20:39 -0500

    added export function

--- recipe-app-cll/src/components/AccountSettings/index.js
+++ recipe-app-cll/src/components/AccountSettings/index.js
@@ -59,7 +59,7 @@ export class ModalExample extends React.Component {
     return (
       <div>
         <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} size={propr.size ? prop.size : 'lg'}>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Account Settings</ModalHeader>
           <ModalBody>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -85,12 +85,12 @@ export class ModalExample extends React.Component {
   }
 }
 
-// export default class AccountSettings extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//       <ModalExample></ModalExample>
-//       </div>
-//     );
-//   }
-// }
+export default class AccountSettings extends React.Component {
+  render() {
+    return (
+      <div>
+      <ModalExample></ModalExample>
+      </div>
+    );
+  }
+}
