74757450ec8fa3360ffafab1d6b0ce52338e24b6
Thanita Edu <thanitag.io@gmail.com>
2019-01-31 17:14:28 -0500

    remove nav add profile modal

--- recipe-app-cll/src/components/AccountSettings/index.js
+++ recipe-app-cll/src/components/AccountSettings/index.js
@@ -1,32 +1,5 @@
 import React, { Component } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-
-// nav
-
-export class Menu extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>Account Settings</p>
-        <Nav>
-          <NavItem>
-            <NavLink href="#profileModal">Profile</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#recipeSettingsModal">Recipe Settings</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Privacy</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Account</NavLink>
-          </NavItem>
-        </Nav>
-      </div>
-    );
-  }
-}
 
 // modal with nested modal
 
@@ -69,11 +42,30 @@ export class ModalExample extends React.Component {
       <div>
         <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={this.toggle}>Profile</ModalHeader>
+          <ModalBody>
+            Profile info
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Save</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} size={propr.size ? prop.size : 'lg'}>
+          <ModalHeader toggle={this.toggle}>Account Settings</ModalHeader>
           <ModalBody>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <br />
-            <Button color="success" onClick={this.toggleNested}>Show Nested Modal</Button>
+            <Button color="success" onClick={this.toggleNested}>Add Recipes</Button>
+            <Button color="success" onClick={this.toggleNested}>Manage Recipes</Button>
             <Modal isOpen={this.state.nestedModal} toggle={this.toggleNested} onClosed={this.state.closeAll ? this.toggle : undefined}>
               <ModalHeader>Nested Modal title</ModalHeader>
               <ModalBody>Stuff and things</ModalBody>
@@ -93,14 +85,12 @@ export class ModalExample extends React.Component {
   }
 }
 
-export default class AccountSettings extends React.Component {
-  render() {
-    return (
-      <div>
-      <Menu>
-      </Menu>
-      <ModalExample></ModalExample>
-      </div>
-    );
-  }
-}
+// export default class AccountSettings extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//       <ModalExample></ModalExample>
+//       </div>
+//     );
+//   }
+// }
