fe2b6c8a92459e79facb2c5e1f5b3c125116e2ae
Andre Pato <anpato1994@gmail.com>
2019-04-20 22:30:47 -0400

    logic for expanding cards

--- recipe-app-cll/src/components/Dashboard/components/Card.js
+++ recipe-app-cll/src/components/Dashboard/components/Card.js
@@ -4,15 +4,40 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 export class CardItem extends Component {
+
+    state = {
+        open:false,
+    }
+
+    toggleCard =()=> {
+        this.setState({
+            open : !this.state.open
+        });
+    } 
+
+    handleCardExpand = () => {
+        this.toggleCard();
+    }
+
     render(){
+        const cardExpand = this.state.open ? 'card expanded' : 'card';
+
         return (
-            <div className="card">
+            <div className={cardExpand}>
             <img src={CardImage} alt="Card image cap"></img>
             <div className={CardBody}>
                 <h5 className={CardTitle}>Card title</h5>
                 <p className={CardSubtitle}>Some quick example text to build on the card title and make up the bulk of
                     the card's content.</p>
-                <a href="#" className="toggle-recipe">Click to View</a>
+                    <div class="recipe-contents">
+                <label>Name</label>
+                <label>Name</label>
+                <label>Name</label>
+                <label>Name</label>
+                <label>Name</label>
+                <label>Name</label>
+                </div>
+                <button className="toggle-recipe" onClick={this.handleCardExpand}>Click to View</button>
             </div>
         </div>
         )
